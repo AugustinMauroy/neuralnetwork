c55958522fcf3030f57c5f35478b19ecafa2532b
Augustin Mauroy <augustin.mauroy@outlook.fr>
2023-08-03 01:21:39 +0200

    feat(chatbot): new data engin

--- chatbot/index.js
+++ chatbot/index.js
@@ -1,16 +1,41 @@
+import fs from 'node:fs';
+import path from 'node:path';
 import Chatbot from './lib/chatbot.js';
-import data from './data.json' assert { type: 'json' };
 
-// Preprocess the data
-const preprocessedData = data.map(item => ({
-  input: item.question.toLowerCase(), // Convert input to lowercase for case-insensitive matching
-  output: item.answer
-}));
+// Function to read and preprocess a JSON file
+const readAndPreprocessJSON = (filePath) => {
+	const data = fs.readFileSync(filePath, 'utf8');
+	const jsonData = JSON.parse(data);
+	return jsonData.map(item => ({
+		input: item.question.toLowerCase(),
+		output: item.answer
+	}));
+};
+
+
+// Function to read all JSON files in the 'data' directory
+const readAllDataFiles = () => {
+	const dataDirectory = path.join(path.resolve(), 'data');
+	const dataFiles = fs.readdirSync(dataDirectory).filter(file => file.endsWith('.json'));
+	const allData = [];
+	dataFiles.forEach(file => {
+		const filePath = path.join(dataDirectory, file);
+		const fileData = readAndPreprocessJSON(filePath);
+		allData.push(...fileData);
+	});
+	return allData;
+};
+
+// Get all preprocessed data from all JSON files
+const allPreprocessedData = readAllDataFiles();
+
+console.log(allPreprocessedData);
+
 
 // Train the chatbot
 const chatbot = new Chatbot();
-preprocessedData.forEach(item => {
-  chatbot.addDocument(item.input, item.output);
+allPreprocessedData.forEach(item => {
+	chatbot.addDocument(item.input, item.output);
 });
 chatbot.train();
 
@@ -18,4 +43,4 @@ chatbot.train();
 //chatbot.REPLstart();
 
 // Start API server
-//chatbot.APIstart();
+// chatbot.APIstart();
