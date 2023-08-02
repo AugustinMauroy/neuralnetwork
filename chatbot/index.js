68ee6291037dec32b18aae8b7e390eb980d98700
Augustin Mauroy <augustin.mauroy@outlook.fr>
2023-08-03 00:23:26 +0200

    feat(chatbot): make it more robust

--- chatbot/index.js
+++ chatbot/index.js
@@ -3,14 +3,14 @@ import data from './data.json' assert { type: 'json' };
 
 // Preprocess the data
 const preprocessedData = data.map(item => ({
-	input: item.question,
-	output: item.answer
+  input: item.question.toLowerCase(), // Convert input to lowercase for case-insensitive matching
+  output: item.answer
 }));
 
 // Train the chatbot
 const chatbot = new Chatbot();
 preprocessedData.forEach(item => {
-	chatbot.addDocument(item.input, item.output);
+  chatbot.addDocument(item.input, item.output);
 });
 chatbot.train();
 
--- chatbot/lib/chatbot.js
+++ chatbot/lib/chatbot.js
@@ -5,60 +5,61 @@ const textColor = (colorCode, text) => `\x1b[${colorCode}m${text}\x1b[0m`;
 
 class Chatbot {
 	constructor() {
-		this.classifier = natural.BayesClassifier();
-		this.rl = readline.createInterface({
-		  input: process.stdin,
-		  output: process.stdout
-		});
-		this.context = []; // Initialize an array to store the conversation context
-	  }
-
+	  this.classifier = natural.BayesClassifier();
+	  this.rl = readline.createInterface({
+			input: process.stdin,
+			output: process.stdout
+	  });
+	  this.context = []; // Initialize an array to store the conversation context
+	}
+  
 	addDocument(doc, category) {
-		// Add validation and sanitization here if needed
-		this.classifier.addDocument(doc, category);
+	  // Add validation and sanitization here if needed
+	  this.classifier.addDocument(doc, category);
 	}
-
+  
 	train() {
-		this.classifier.train();
+	  this.classifier.train();
 	}
-
+  
 	classify(doc) {
-
-		try {
-			const processedInput = doc.toLowerCase();
+	  try {
+			const processedInput = doc.toLowerCase().replace(/[^\w\s]/gi, '');
 			const responseCategories = this.classifier.classify(processedInput);
-	
+  
 			// Store the current user input and chatbot response in the context
 			this.context.push({ input: doc, output: responseCategories[0] });
-	
+  
 			// Limit the context to a reasonable number of interactions (e.g., 5)
 			const maxContextSize = 5;
 			if (this.context.length > maxContextSize) {
 		  this.context.shift(); // Remove the oldest element from the context
 			}
-	
+  
 			// If the classifier is not confident, return a default response or ask for clarification.
-			const ambiguous = responseCategories.length === 1 && maxProb < 0.5;
+			const ambiguous = responseCategories.length === 1 && this.classifier.maxProb < 0.5;
 			return ambiguous ? 'I\'m not sure. Can you please provide more context?' : responseCategories[0];
-		} catch (error) {
+	  } catch (error) {
 			console.error(`Error occurred while processing the input: ${error.message}`);
 			return 'Oops! Something went wrong. Please try again.';
-		}
 	  }
-
+	}
+  
 	start() {
-		const prompt = () => {
+	  const prompt = () => {
 			this.rl.question(textColor(32, '[You]: '), input => {
-				// Store the user's input in the context (before processing for classification)
-				this.context.push({ input });
-    
-				const output = this.classify(input);
-				console.log(`${textColor(31, '[Chatbot]:')} ${output}`);
-				prompt();
+		  // Remove punctuation from the user's input
+		  const sanitizedInput = input.replace(/[^\w\s]/gi, '');
+		  // Store the user's input in the context (before processing for classification)
+		  this.context.push({ input });
+  
+		  const output = this.classify(sanitizedInput);
+		  console.log(`${textColor(31, '[Chatbot]:')} ${output}`);
+		  prompt();
 			});
-		};
-    
-		prompt();
+	  };
+  
+	  prompt();
 	}
 }
 
