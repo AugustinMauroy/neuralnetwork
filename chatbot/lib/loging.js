bc97fdf3cb7fe265574eb7129c6bb15ca0770585
Augustin Mauroy <augustin.mauroy@outlook.fr>
2023-08-03 23:25:45 +0200

    fix: lint

--- chatbot/lib/loging.js
+++ chatbot/lib/loging.js
@@ -1,26 +1,26 @@
 export const textColor = (colorCode, text) => `\x1b[${colorCode}m${text}\x1b[0m`;
 
 function info (message) {
-    console.warn(`${textColor(32, '[INFO]:')} ${message}`);
+	console.warn(`${textColor(32, '[INFO]:')} ${message}`);
 }
 
 function error (message) {
-    console.warn(`${textColor(31, '[ERROR]:')} ${message}`);
+	console.warn(`${textColor(31, '[ERROR]:')} ${message}`);
 }
 
 function warn (message) {
-    console.warn(`${textColor(33, '[WARNING]:')} ${message}`);
+	console.warn(`${textColor(33, '[WARNING]:')} ${message}`);
 }
 
 function debug (message) {
-    console.warn(`${textColor(34, '[DEBUG]:')} ${message}`);
+	console.warn(`${textColor(34, '[DEBUG]:')} ${message}`);
 }
 
 const log = {
-    info,
-    error,
-    warn,
-    debug
+	info,
+	error,
+	warn,
+	debug
 };
 
 export { log };
